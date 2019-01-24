c619a714a91aa8051a1e38b47543dafdde6ede48
cutkit49 <tungld702@gmail.com>
2019-01-24 10:51:06 +0700

    Add timer

--- UsingWebSocket/MicrosoftWebSockets.cs
+++ UsingWebSocket/MicrosoftWebSockets.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Timers;
 using System.Web;
 
 namespace UsingWebSocket
@@ -10,12 +11,21 @@ namespace UsingWebSocket
     {
         private static WebSocketCollection clients = new WebSocketCollection();
         private string name;
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            clients.Broadcast("hello");
+        }
         public override void OnOpen()
         {
             //base.OnOpen();
             name = this.WebSocketContext.QueryString["chatName"];
             clients.Add(this);
             clients.Broadcast(name + " has connected!");
+
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 1000;
+            aTimer.Enabled = true;
         }
         public override void OnMessage(string message)
         {
